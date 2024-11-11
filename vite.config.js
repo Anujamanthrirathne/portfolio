eb52e05788eb7fb9b9d37e15b91e73c5ed5a8e8b
anuja <anujamanthriratne@gmail.com>
2024-11-11 08:01:33 -0800

    aaa

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,9 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/portfolio/',  // Adjust this to your actual Vercel deployment path
   plugins: [react()],
   build: {
-    outDir: 'dist', // Ensure Vercel knows where to look for build output
-    rollupOptions: {
-      output: {
-        // Customize how the chunks are split
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor'; // Bundle all third-party modules into a vendor chunk
-          }
-        },
-      },
-    },
+    outDir: 'dist'
   },
-  // Optional: Set base path if deploying under a subfolder
-  base: '/', // Adjust this if your project is hosted under a subfolder
 });
