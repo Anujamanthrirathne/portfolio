df4f7454c1f7cd2f749c7d79536ac828d8a99ec7
anuja <anujamanthriratne@gmail.com>
2024-11-11 08:46:34 -0800

    aneeh

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',  // Ensure the base path is correct, especially if the app is deployed in a subfolder
-  plugins: [react()],
   build: {
-    outDir: 'dist',  // Make sure the output goes into the 'dist' folder
+    rollupOptions: {
+      external: ['/assets/main-BqibTNCe.js'], // Externalize the asset
+    },
   },
 });
