bf90aa6e1929879c2a16b44252e344e88c219a71
anuja <anujamanthriratne@gmail.com>
2024-11-11 09:31:54 -0800

    jlouip

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/subfolder/',  // Matches the subfolder where the app is deployed
+  base: '/subfolder/',  // Matches the deployed subfolder
   plugins: [react()],
   build: {
-    outDir: 'dist',  // Ensures the build files are outputted in the 'dist' folder
+    outDir: 'dist',
   },
 });
