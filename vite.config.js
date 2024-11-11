d44985a613d55da03c7024fc6972a7f7c8de35e5
anuja <anujamanthriratne@gmail.com>
2024-11-11 09:10:40 -0800

    sdsaffdssadsfd

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'https://portfolio-3h6g.vercel.app/',  // Set the base path to your deployed URL
+  base: '/',  // This is correct if your app is at the root of the domain
   plugins: [react()],
   build: {
-    outDir: 'dist',  // Ensure the output goes into the 'dist' folder
+    outDir: 'dist',  // Build output will be placed in 'dist' folder
   },
 });
