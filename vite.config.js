96563a35ca6df9179f2dea353e4e5d0f114ebf8d
anuja <anujamanthriratne@gmail.com>
2024-11-12 07:47:31 -0800

    pppppp

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // Ensure this matches the `distDir` in vercel.json
     rollupOptions: {
       output: {
         entryFileNames: 'assets/[name]-[hash].js',
