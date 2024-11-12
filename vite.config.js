838fa6b95eb99b601eeaecc80dd2717ecc002e2d
anuja <anujamanthriratne@gmail.com>
2024-11-12 07:29:40 -0800

    poist

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // Use a relative base path
   plugins: [react()],
   build: {
-    outDir: 'dist', // Output directory is 'dist'
+    outDir: 'dist',
     rollupOptions: {
       output: {
-        entryFileNames: 'assets/index-[hash].js', // Ensure hashed JS file names
-        chunkFileNames: 'assets/[name]-[hash].js', // Ensure chunk JS file names
-        assetFileNames: 'assets/[name]-[hash][extname]' // Ensure asset file names are hashed
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]'
       }
     }
   }
