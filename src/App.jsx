5d5ee1e67fc070b926fbb57a1678a1a1fbbc2521
anuja <anujamanthriratne@gmail.com>
2024-11-13 02:44:29 -0800

    poistions

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,33 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Suspense, lazy } from "react";
-
-// Lazy load components to improve performance
-const About = lazy(() => import("./components/About"));
-const Contact = lazy(() => import("./components/Contact"));
-const Experience = lazy(() => import("./components/Experience"));
-const Feedbacks = lazy(() => import("./components/Feedbacks"));
-const Hero = lazy(() => import("./components/Hero"));
-const Navbar = lazy(() => import("./components/Navbar"));
-const Tech = lazy(() => import("./components/Tech"));
-const Works = lazy(() => import("./components/Works"));
-const StarsCanvas = lazy(() => import("./components/canvas/Stars"));
+// Temporary: Replace lazy loading with normal imports
+import About from './components/About';
+import Contact from './components/Contact';
+import Experience from './components/Experience';
+import Feedbacks from './components/Feedbacks';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Tech from './components/Tech';
+import Works from './components/Works';
+import StarsCanvas from './components/canvas/Stars';
 
 const App = () => {
   return (
     <BrowserRouter>
-    <div className='relative z-0 bg-primary'>
-      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-        <Navbar />
-        <Hero />
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
       </div>
-      <About />
-      <Experience />
-      <Tech />
-      <Works />
-      <Feedbacks />
-      <div className='relative z-0'>
-        <Contact />
-        <StarsCanvas />
-      </div>
-    </div>
-  </BrowserRouter>
+    </BrowserRouter>
   );
 };
 
