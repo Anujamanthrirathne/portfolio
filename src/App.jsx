aeab9fd79396d2d98482849c9ee432ab910a8a2e
anuja <anujamanthriratne@gmail.com>
2024-11-13 02:04:47 -0800

    otrsf

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,28 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Suspense, lazy } from "react";
 
-// Lazy load components to improve performance
-const About = lazy(() => import("./components/About"));
-const Contact = lazy(() => import("./components/Contact"));
-const Experience = lazy(() => import("./components/Experience"));
-const Feedbacks = lazy(() => import("./components/Feedbacks"));
-const Hero = lazy(() => import("./components/Hero"));
-const Navbar = lazy(() => import("./components/Navbar"));
-const Tech = lazy(() => import("./components/Tech"));
-const Works = lazy(() => import("./components/Works"));
-const StarsCanvas = lazy(() => import("./components/canvas/Stars"));
+import { BrowserRouter } from "react-router-dom";
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
-        <div className="relative z-0 bg-primary">
-          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-            <Navbar />
-            <Hero />
-          </div>
-
-          <Routes>
-            <Route path="/" element={<About />} />
-            <Route path="/experience" element={<Experience />} />
-            <Route path="/tech" element={<Tech />} />
-            <Route path="/works" element={<Works />} />
-            <Route path="/feedbacks" element={<Feedbacks />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-
-          <div className="relative z-0">
-            <StarsCanvas />
-          </div>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
         </div>
-      </Suspense>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
